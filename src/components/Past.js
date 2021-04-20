a1fa8638c36f954d8a6f660ebcec0f63c3045bcc
Ryan Garren <rryangarren@gmail.com>
2021-04-20 11:52:06 -0600

    3 views and db and server up and running

---
+++ src/components/Past.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Past extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Past page</h1>
+      </div>
+    );
+  };
+};
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+  import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Landing from './components/Landing';
+import Today from './components/Today';
+import Past from './components/Past';
+
+export default (
+  <Switch>
+    <Route component={Landing} exact path='/' /> 
+    <Route component={Today} path='/today' />
+    <Route component={Past} path='/past' />
+  </Switch>
+);
