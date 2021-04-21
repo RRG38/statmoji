6230941591f6dbda0369f82d2ddbdc55fb46cfb4
Ryan Garren <rryangarren@gmail.com>
2021-04-21 11:43:22 -0600

    landing page front end complete

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <BrowserRouter>
       <div>
-        <div>Statmoji</div>
         {routes}
       </div>
     </BrowserRouter>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import './Landing.css'
 
 export default class Landing extends Component {
   render() {
     return (
       <div>
-        <h1>Landing page</h1>
+        <h1>Statmoji</h1>
+        <h2>Your Life Stats Emojified</h2>
+        <img src="https://cdn.shopify.com/s/files/1/1061/1924/products/Smiling_Face_Emoji_large.png?v=1571606036" alt="Smiley Face"/>
+        <span class="email">
+          <label>Email</label>
+            <input
+                  type='text'
+                  // value={this.state.username}
+                  placeholder='email'
+                  name='email'
+                  // onChange={this.handleInputChange}
+            />
+        </span>
+        <span class="password">
+          <label>Password</label>
+            <input
+                  type='text'
+                  // value={this.state.password}
+                  placeholder='password'
+                  name='password'
+                  // onChange={this.handleInputChange}
+            />
+        </span>
+        <span class="buttons">
+          <button onClick={this.handleLogin}>Login</button>
+          <button onClick={this.handleRegister}>Register</button>
+        </span>
       </div>
     );
   };
