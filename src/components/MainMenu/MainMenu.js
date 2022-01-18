1310ce2fe96e86cc0faf298c207824a95980e420
Ryan Garren <rryangarren@gmail.com>
2022-01-18 13:01:38 -0700

    finished resp. design for menu, add to all

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -6,27 +6,18 @@ const MainMenu = () => {
   return (
     <div>
       <Nav />
-      <div className='menu-title'> Menu </div>
-      <div className='top-menu-content'>
+      <div className='main-menu-title'>
+        <div> Menu </div>
+      </div>
+      <div className='main-menu-content'>
         <button className='main-menu-button'> ADD FOOD/DRINK STATS </button>
-        <br></br>
         <button className='main-menu-button' > ADD ACTIVITIES STATS </button>
-        <br></br>
         <button className='main-menu-button'> ADD HOW I'M FEELING STATS </button>
-        <br></br>
         <button className='main-menu-button'   > VIEW ALL MY PAST STATS </button>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <button className='main-menu-button'   > ABOUT STATMOJI </button><br></br>
+        <button className='main-menu-button'   > ABOUT STATMOJI </button>
+        <button className='main-menu-button' > CHANGE COLOR PREFERENCES </button>
         <button className='main-menu-button'   > CONTACT US </button>
-        <br></br>
         <button className='main-menu-button'   > BECOME A SUPPORTER </button>
-        <br></br>
       </div>
     </div>
   )
